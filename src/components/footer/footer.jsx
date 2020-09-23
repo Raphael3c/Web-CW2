af100cc8550b888bff209bbad29d486a2dbfff4a
bzrra <victorbzrra@alu.ufc.br>
2020-09-22 21:16:29 -0300

    Commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,9 @@ import './footer.css';
 function footer(){
     return(
     <footer>
-        Logo: <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com</a>
+        Logo: <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from: <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com</a>
+        <br></br> 
+        Creditos: <a href="https://www.instagram.com/orbisidus/" title="Raphael">Raphael</a> e <a href="https://www.instagram.com/bzerra.a/" title="Victor">Victor</a>.
     </footer>);
 }
 
